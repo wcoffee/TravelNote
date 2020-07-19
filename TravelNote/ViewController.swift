bc1c895de3293f8d27030e115d5d42e2fce306e3
FC <wcoffee995@gmail.com>
2020-07-19 21:49:40 +0800

    Initial Commit

---
+++ TravelNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelNote
+//
+//  Created by Freda Cheng on 2020/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
